884604ea0a050ab2aec57897ad070d83f77cc514
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2021-11-10 10:09:28 +0000

    null message

--- myProject/settings.py
+++ myProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +154,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
