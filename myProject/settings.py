88cb662b0f45e5c050d7b724085d0567335e9b0a
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2021-11-10 13:02:44 +0000

    new commit

--- myProject/settings.py
+++ myProject/settings.py
@@ -154,6 +154,9 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
 }
-
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
